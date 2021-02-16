d5953d7646f93ea9eaec0f8b1c9b1d4d1be67d54
Marco Baltazar <Marco.Baltazar@highjump.com>
2021-02-16 09:21:08 -0400

    create utils for fuse searcher

---
+++ src/utils/utils.js
@@ -0,0 +1,35 @@
+import Fuse from 'fuse.js';
+
+export const buildArray = (list) => {
+  return list.map((item) => ({
+    _id: item._id,
+    name: item.name,
+    email: item.email,
+    cellphone: item.cellphone,
+    workarea: item.workarea,
+    status: item.status,
+    description: item.description,
+    knowledge: item.knowledge,
+  }));
+};
+
+export const fuse = (text, list) => {
+  const options = {
+    // isCaseSensitive: false,
+    // includeScore: false,
+    // includeMatches: true,
+    // shouldSort: true,
+    // findAllMatches: false,
+    // minMatchCharLength: 1,
+    // location: 0,
+    // threshold: 0.6,
+    // distance: 100,
+    // useExtendedSearch: false,
+    // ignoreLocation: false,
+    // ignoreFieldNorm: false,
+    keys: ['name', 'email', 'workarea'],
+  };
+  const response = new Fuse(list, options);
+  const pattern = text;
+  return response.search(pattern);
+};
