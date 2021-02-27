4378d1902e148bbe2f7bd94a0e4bc46867f7cff9
Marco Baltazar <Marco.Baltazar@highjump.com>
2021-02-27 05:41:37 -0400

    change Search component to class component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,75 +1,166 @@
-import React, { useEffect, useState } from 'react';
+import * as React from 'react';
 import { Alert } from 'react-native';
+import _ from 'lodash';
 
-import { SUBSCRIPTION_USER_ADDED } from '../../graphql/user/user.graphql';
-import { getAllUsers, deleteUser } from '../../graphql/user/user.api';
+// import { SUBSCRIPTION_USER_ADDED } from '../../graphql/user/user.graphql';
+import {
+  getAllUsers,
+  getPersonalInformation,
+} from '../../graphql/user/user.api';
+import {
+  getAllContacts,
+  createNewContact,
+} from '../../graphql/contact/contact.api';
+
 import ActivityIndicatorEJ from '../../core-components/ActivityIndicatorEJ/ActivityIndicatorEJ';
 import EmptyDataEJ from '../../core-components/EmptyData/EmptyDataEJ';
 import ListEJ from '../../core-components/ListEJ/ListEJ';
-import localization from '../../localization/localization';
-import client from '../../service/setup';
+import HeaderEJ from '../../core-components/HeaderEJ/HeaderEJ';
 
 import * as localStorage from '../../config/local-storage/localStorage';
+import localization from '../../localization/localization';
+import * as utils from '../../utils/utils';
+// import client from '../../service/setup';
 
-const Search = () => {
-  const [users, setUsers] = useState([]);
-  const [currentUser, setCurrentUser] = useState(undefined);
-  // const [search, setSearch] = useState({
-  //   email: { contains: '@' },
-  // });
-  const [search, setSearch] = useState(undefined);
-  const [isLoading, setIsLoading] = useState(true);
+export default class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        _id: '',
+        name: '',
+        email: '',
+        cellphone: '',
+        workarea: '',
+        status: undefined,
+        description: '',
+        knowledge: '',
+      },
+      users: [],
+      contacts: [],
+      currentUser: {},
+      isLoading: false,
+      search: undefined,
+      searchText: '',
+    };
+  }
 
-  const updateUserList = ({ data }) => {
-    setUsers((currentUsers) => {
-      return [...currentUsers, data.userAdded];
-    });
+  componentDidMount = async () => {
+    await this.isUserLogged();
+    await this.getUsers();
   };
 
-  useEffect(() => {
-    const isUserLogged = async () => {
-      try {
-        const token = await localStorage.getStringValue('token');
-        setCurrentUser(token);
-      } catch (e) {
-        console.log(e.message);
+  // componentDidUpdate = () => {
+  //   client
+  //     .subscribe({
+  //       query: SUBSCRIPTION_USER_ADDED,
+  //     })
+  //     .subscribe({
+  //       next: (data) => {
+  //         this.updateUserList(data);
+  //       },
+  //       error: (err) => {
+  //         console.log(err);
+  //       },
+  //     });
+  // };
+
+  isUserLogged = async () => {
+    try {
+      const storedSignInDetails = await localStorage.getStringValue('token');
+      if (storedSignInDetails !== null) {
+        this.setState({ currentUser: storedSignInDetails });
+        await this.userInformation(storedSignInDetails);
       }
-    };
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
 
-    isUserLogged();
-  }, []);
-
-  useEffect(() => {
-    getUsers();
-
-    client
-      .subscribe({
-        query: SUBSCRIPTION_USER_ADDED,
-      })
-      .subscribe({
-        next: (data) => {
-          updateUserList(data);
-        },
-        error: (err) => {
-          console.log(err);
+  userInformation = async (value) => {
+    try {
+      const { getUserByString } = await getPersonalInformation({
+        token: value,
+      });
+      this.setState({
+        user: {
+          _id: getUserByString._id,
+          name: getUserByString.name,
+          email: getUserByString.email,
+          cellphone: getUserByString.cellphone,
+          workarea: getUserByString.workarea,
+          status: getUserByString.status,
+          description: getUserByString.description,
+          knowledge: getUserByString.knowledge,
         },
       });
-  });
+      await this.getContacts(getUserByString._id);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
 
-  const getUsers = async () => {
+  getContacts = async (_id) => {
+    try {
+      const { data, loading } = await getAllContacts(_id);
+      this.setState({ contacts: data.getContactsByUserId });
+      this.setState({ isLoading: loading });
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  updateUserList = ({ data }) => {
+    // setUsers((currentUsers) => {
+    //   return [...currentUsers, data.userAdded];
+    // });
+  };
+
+  getUsers = async () => {
+    const { search, currentUser, contacts } = this.state;
     try {
       const { data, loading } = await getAllUsers(search, currentUser);
-      setUsers(data.getUsers);
-      setIsLoading(loading);
+      const usersFiltered = _.differenceBy(
+        utils.buildArray(data.getUsers),
+        utils.buildArray(contacts),
+        'email',
+      );
+      this.setState({
+        users: usersFiltered,
+        isLoading: loading,
+        searchText: '',
+      });
     } catch (error) {
       console.log(error.message);
     }
   };
 
-  const onPressItem = (user) => {
-    const knowledge = user.email || '';
+  onPressAddNewUserIcon = async (value) => {
+    const { users, user } = this.state;
+    const newList = users.filter((item) => item._id !== value._id);
+    try {
+      const contact = {
+        _id: value._id,
+        createdBy: user._id,
+        name: value.name,
+        email: value.email,
+        cellphone: value.cellphone,
+        workarea: value.workarea,
+        status: value.status,
+        description: value.description,
+        knowledge: value.knowledge,
+      };
+      await createNewContact(contact);
+      this.setState({ users: newList });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  onPressItem = (value) => {
+    const knowledge = value.email || '';
     Alert.alert(
-      user.name,
+      value.name,
       `${localization.KnowledgeContact}: ${knowledge}`,
       [
         {
@@ -80,31 +171,38 @@ const Search = () => {
     );
   };
 
-  const onPressDeleteIcon = async (user) => {
-    try {
-      await deleteUser(user._id);
-    } catch (error) {
-      console.log(error.message);
+  handlerSearch = async (text) => {
+    const { users } = this.state;
+
+    if (text.length === 0) {
+      await this.getUsers();
+    } else {
+      this.setState({
+        users: utils.fuse(text, users).map((element) => element.item),
+        searchText: text,
+      });
     }
   };
 
-  if (isLoading) {
-    return <ActivityIndicatorEJ />;
+  render() {
+    const { isLoading, users, searchText } = this.state;
+    if (isLoading) {
+      return <ActivityIndicatorEJ />;
+    }
+    if (users.length === 0 && searchText.length === 0) {
+      return <EmptyDataEJ />;
+    }
+    return (
+      <>
+        <HeaderEJ searchText={searchText} handlerSearch={this.handlerSearch} />
+        <ListEJ
+          data={users}
+          leftIcon="user-o"
+          rightIcon="user-plus"
+          onPressItem={this.onPressItem}
+          onPressRightIcon={this.onPressAddNewUserIcon}
+        />
+      </>
+    );
   }
-
-  const emptyRender = () => <EmptyDataEJ />;
-
-  const listRender = (data) => (
-    <ListEJ
-      data={users}
-      leftIcon="user-o"
-      rightIcon="user-plus"
-      onPressItem={onPressItem}
-      onPressRightIcon={onPressDeleteIcon}
-    />
-  );
-
-  return users ? listRender(users) : emptyRender();
-};
-
-export default Search;
+}
