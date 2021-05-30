a4f0c661fc6e434206316efbe8c8bf05572fcb56
Marco Baltazar <Marco.Baltazar@highjump.com>
2021-05-30 16:35:09 -0400

    add action to remove externa user id on onesignal

--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Text } from 'native-base';
+import OneSignal from 'react-native-onesignal';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import {
@@ -20,8 +21,27 @@ import styles from './Stack.style';
 const Stack = createStackNavigator();
 
 const StackScreen = () => {
+  const [isExternalUserIdPushed, setIsExternalUserIdPushed] = useState(false);
+
   const goBack = (navigation) => {
     localStorage.removeMultipleValues(['language', 'token']);
+    OneSignal.removeExternalUserId((results) => {
+      // The results will contain push and email success statuses
+      // console.log('Results of removing external user id');
+      // console.log(results);
+      // // Push can be expected in almost every situation with a success status, but
+      // // as a pre-caution its good to verify it exists
+      if (results.push && results.push.success) {
+        //   console.log('Results of removing external user id push status:');
+        //   console.log(results.push.success);
+        setIsExternalUserIdPushed(results.push.success);
+      }
+      // // Verify the email is set or check that the results have an email success status
+      // if (results.email && results.email.success) {
+      //   console.log('Results of removoing external user id email status:');
+      //   console.log(results.email.success);
+      // }
+    });
     navigation.navigate('SignIn');
   };
 
